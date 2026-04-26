012a98d0e89a3b329819dc8940995eb265b3f1b7
cvardhan501 <cvardhan501@gmail.com>
2026-04-26 19:12:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const axios = require("axios");
-// const cors = require("cors");
-
-// const app = express();
-// app.use(cors());
-
-// const PORT = 5000;
-// const API_KEY = process.env.API_KEY;
-
-// app.get("/weather", async (req, res) => {
-//   const city = req.query.city;
-
-//   if (!city) {
-//     return res.status(400).json({ error: "City is required" });
-//   }
-
-//   try {
-//     const response = await axios.get(
-//       "https://api.openweathermap.org/data/2.5/weather",
-//       {
-//         params: {
-//           q: city,
-//           appid: API_KEY,
-//           units: "metric",
-//         },
-//       }
-//     );
-
-//     res.json(response.data);
-//   } catch (error) {
-//     console.log("FULL ERROR:", error.response?.data);
-
-//     res.status(500).json({
-//       error: error.response?.data?.message || "Something went wrong",
-//     });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running on http://localhost:${PORT}`);
-// });
-// app.get("/forecast", async (req, res) => {
-//   const city = req.query.city;
-
-//   try {
-//     const response = await axios.get(
-//       "https://api.openweathermap.org/data/2.5/forecast",
-//       {
-//         params: {
-//           q: city,
-//           appid: API_KEY,
-//           units: "metric",
-//         },
-//       }
-//     );
-
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Forecast error" });
-//   }
-// });
 require("dotenv").config();
 
 const express = require("express");
@@ -119,6 +55,15 @@ app.get("/forecast", async (req, res) => {
     });
   }
 });
+const path = require("path");
+
+// ✅ serve frontend
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "weather_ui.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
